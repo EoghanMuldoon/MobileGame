2773a3e7c52b289e8133b796c92110daf91329d4
Eoghan Muldoon <G00358767@gmit.ie>
2020-02-26 12:57:22 +0000

    Game with own player and mouse

---
+++ Assets/__Scripts/Enemy.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 1.0f;
+
+    private Rigidbody2D rb;
+    private GameObject player;
+    private float enemyMoveX;
+    private float enemyMoveY;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+        fellowPlayer();
+    }
+
+    private void OnTriggerEnter2D(Collider2D whatHitMe)
+    {
+        // parameter = what ran into me
+        // if the player hit, then destroy the player
+        // and the current enemy rectangle
+
+        var player = whatHitMe.GetComponent<PlayerMovement>();
+        
+
+        Debug.Log("Hit Something");
+
+        if (player)  // if (player != null)
+        {
+            Debug.Log("It was the player");
+            // play crash sound here
+            // destroy the player and the rectangle
+            // give the player points/coins
+            Destroy(player.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    void fellowPlayer()
+    {
+        
+        enemyMoveX = transform.position.x - player.transform.position.x;
+        enemyMoveY = transform.position.y - player.transform.position.y;
+
+
+        if (enemyMoveX < 0 && enemyMoveY < 0)
+        {
+            rb.velocity = new Vector2(1 * speed, 1 * speed);
+        }
+        else if (enemyMoveX > 0 && enemyMoveY < 0)
+        {
+            rb.velocity = new Vector2(-1 * speed, 1 * speed);
+        }
+        else if (enemyMoveX < 0 && enemyMoveY > 0)
+        {
+            rb.velocity = new Vector2(1 * speed, -1 * speed);
+        }
+        else if (enemyMoveX > 0 && enemyMoveY > 0)
+        {
+            rb.velocity = new Vector2(-1 * speed, -1 * speed);
+        }
+
+    }
+}
---
+++ Assets/__Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // == public fields ==
+
+    // == private fields ==
+    private Rigidbody2D rb;
+
+    private string Horizontal = "Horizontal";
+    private string Vertical = "Vertical";
+    private float yBottomClamp= -4.7f;
+    private float yTopClamp= 4.5f;
+    private float xLeftClamp= -8.4f;
+    private float xRightClamp= 8.5f;
+
+
+    [SerializeField] private float speed = 7.0f;
+    // == private methods ==
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveController();
+        faceMouse();
+    }
+
+    void MoveController()
+    {
+        float vMovement = Input.GetAxis(Vertical);
+        float hMovement = Input.GetAxis(Horizontal);
+
+        rb.velocity = new Vector2(hMovement * speed,
+                             vMovement * speed);
+
+
+        // keep the player on the screen
+        // float xValue = Mathf.clamp(value, min, max)
+        // rb.position.x 
+        float yValue = Mathf.Clamp(rb.position.y, yBottomClamp, yTopClamp);
+        float xValue = Mathf.Clamp(rb.position.x, xLeftClamp, xRightClamp);
+
+        rb.position = new Vector2(xValue, yValue);
+    }
+
+    void faceMouse()
+    {
+
+        //Code from https://www.youtube.com/watch?v=_XdqA3xbP2A
+
+
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x,
+                                        mousePosition.y - transform.position.y);
+
+        transform.up = direction;
+    }
+}
